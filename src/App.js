f097f4f5cbffbe3b847c6b5fdcf0fddcd10e97fe
Dolgirev Aleksey <ado-stalr@mail.ru>
2021-06-28 12:27:33 +0300

    my first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World! This is my test react App
         </a>
       </header>
     </div>
